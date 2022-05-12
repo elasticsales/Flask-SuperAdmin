5bc6647d85f7c85c57d351ff5aa1809cecb279db
Alec Rosenbaum <alec.rosenbaum@close.io>
2022-05-12 15:08:17 -0400

    fix print lines causing warnings

--- doc/conf.py
+++ doc/conf.py
@@ -273,12 +273,12 @@ intersphinx_mapping = {"http://docs.python.org/": None}
 try:
     __import__("flask_theme_support")
 except ImportError, e:
-    print "-" * 74
-    print "Warning: Flask themes unavailable. Building with default theme"
-    print "If you want the Flask themes, run this command and build again:"
-    print
-    print " git submodule update --init"
-    print "-" * 74
+    print("-" * 74)
+    print("Warning: Flask themes unavailable. Building with default theme")
+    print("If you want the Flask themes, run this command and build again:")
+    print()
+    print(" git submodule update --init")
+    print("-" * 74)
 
     pygments_style = "tango"
     html_theme = "default"
